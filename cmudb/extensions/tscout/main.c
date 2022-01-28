1e7c5fc33e26330e492e84ade4e79c4a42d9bf4d
Karthik Ramanathan <karthikram.3006@gmail.com>
2022-01-28 04:19:58 -0500

    Run clang-format.
    Signed-off-by: Wan Shen Lim <wanshen.lim@gmail.com>

--- cmudb/extensions/tscout/main.c
+++ cmudb/extensions/tscout/main.c
@@ -1,22 +1,21 @@
 #include <time.h>
 
-#include "postgres.h"
-#include "fmgr.h"
+#include "../../../src/include/tscout/marker.h"
 #include "commands/createas.h"
 #include "commands/explain.h"
+#include "fmgr.h"
+#include "operating_unit_features.h"
 #include "optimizer/planner.h"
 #include "parser/parsetree.h"
+#include "postgres.h"
 #include "utils/builtins.h"
-#include "../../../src/include/tscout/marker.h"
-#include "operating_unit_features.h"
 
 PG_MODULE_MAGIC;
 void _PG_init(void);
 void _PG_fini(void);
 
-static void ExplainOneQueryWrapper(Query* query, int cursorOptions, IntoClause* into,
-                                   ExplainState* es, const char* queryString,
-                                   ParamListInfo params, QueryEnvironment *queryEnv);
+static void ExplainOneQueryWrapper(Query *query, int cursorOptions, IntoClause *into, ExplainState *es,
+                                   const char *queryString, ParamListInfo params, QueryEnvironment *queryEnv);
 static void WalkPlan(Plan *plan, ExplainState *es);
 static void explainXs(Plan *node, ExplainState *es);
 static size_t get_field_size(c_type type);
@@ -24,213 +23,197 @@ static size_t get_field_size(c_type type);
 static ExplainOneQuery_hook_type chain_ExplainOneQuery = NULL;
 
 void _PG_init(void) {
-    elog(LOG, "Initializing extension");
-    
-    // Chain the Extension Explain Query Wrapper to the head of the chain.
-    chain_ExplainOneQuery = ExplainOneQuery_hook;
-    ExplainOneQuery_hook = ExplainOneQueryWrapper;
+  elog(LOG, "Initializing extension");
 
-    // Init logic (like setting flags) go here
+  // Chain the Extension Explain Query Wrapper to the head of the chain.
+  chain_ExplainOneQuery = ExplainOneQuery_hook;
+  ExplainOneQuery_hook = ExplainOneQueryWrapper;
+
+  // Init logic (like setting flags) go here
 }
 
 void _PG_fini(void) {
-    // Clean up logic goes here
-    ExplainOneQuery_hook = chain_ExplainOneQuery;
-    elog(DEBUG1, "Finishing extension");
+  // Clean up logic goes here
+  ExplainOneQuery_hook = chain_ExplainOneQuery;
+  elog(DEBUG1, "Finishing extension");
 }
 
-static void ExplainOneQueryWrapper(Query* query, int cursorOptions, IntoClause* into,
-                                   ExplainState* es, const char* queryString,
-                                   ParamListInfo params, QueryEnvironment* queryEnv) {
-    
-    PlannedStmt *plan;
-    QueryDesc  *queryDesc;
-    instr_time plan_start, plan_duration;
-    int eflags = 0;
+static void ExplainOneQueryWrapper(Query *query, int cursorOptions, IntoClause *into, ExplainState *es,
+                                   const char *queryString, ParamListInfo params, QueryEnvironment *queryEnv) {
+  PlannedStmt *plan;
+  QueryDesc *queryDesc;
+  instr_time plan_start, plan_duration;
+  int eflags = 0;
 
-    if (chain_ExplainOneQuery) {
-        chain_ExplainOneQuery(query, cursorOptions, into, es,
-                              queryString, params, queryEnv);
-    }
+  if (chain_ExplainOneQuery) {
+    chain_ExplainOneQuery(query, cursorOptions, into, es, queryString, params, queryEnv);
+  }
 
-    // Postgres does not expose an interface to call into the standard ExplainOneQuery.
-    // Hence, we duplicate the operations performed by the standard ExplainOneQuery ie
-    // calling into the standard planner.
-    // A non-standard planner can be hooked in, in the the future.
-    INSTR_TIME_SET_CURRENT(plan_start);
-    plan = (planner_hook ? planner_hook(query, queryString, cursorOptions, params) : 
-    standard_planner(query, queryString, cursorOptions, params));
-    
-    INSTR_TIME_SET_CURRENT(plan_duration);
-    INSTR_TIME_SUBTRACT(plan_duration, plan_start);
+  // Postgres does not expose an interface to call into the standard ExplainOneQuery.
+  // Hence, we duplicate the operations performed by the standard ExplainOneQuery ie
+  // calling into the standard planner.
+  // A non-standard planner can be hooked in, in the the future.
+  INSTR_TIME_SET_CURRENT(plan_start);
+  plan = (planner_hook ? planner_hook(query, queryString, cursorOptions, params)
+                       : standard_planner(query, queryString, cursorOptions, params));
 
-    TS_MARKER("TS_MARKER <NAME>");
-    
-    if (es->format == EXPLAIN_FORMAT_TSCOUT) {
+  INSTR_TIME_SET_CURRENT(plan_duration);
+  INSTR_TIME_SUBTRACT(plan_duration, plan_start);
 
-        queryDesc = CreateQueryDesc(
-            plan,
-            queryString,
-            InvalidSnapshot,
-            InvalidSnapshot,
-            None_Receiver,
-            params,
-            queryEnv,
-            0
-        );
+  TS_MARKER("TS_MARKER <NAME>");
 
-        if (es->analyze)
-            eflags = 0;
-        else
-            eflags = EXEC_FLAG_EXPLAIN_ONLY;
-        if (into)
-            eflags |= GetIntoRelEFlags(into);
-        
-        // Run the executor
-        ExecutorStart(queryDesc, eflags);
-        // This calls into initPlan() which populates the plan tree.
-        // TODO: Create a hook to executor start.
+  if (es->format == EXPLAIN_FORMAT_TSCOUT) {
+    queryDesc =
+        CreateQueryDesc(plan, queryString, InvalidSnapshot, InvalidSnapshot, None_Receiver, params, queryEnv, 0);
 
-        // Finally, walk through the plan, dumping its out in a separate top-level group.
-        ExplainOpenGroup("TscoutProps", NULL, true, es);
-        WalkPlan(queryDesc->planstate->plan, es);
-        ExplainCloseGroup("TscoutProps", NULL, true, es);
+    if (es->analyze)
+      eflags = 0;
+    else
+      eflags = EXEC_FLAG_EXPLAIN_ONLY;
+    if (into) eflags |= GetIntoRelEFlags(into);
 
-        // Free the created query description resources.
-        ExecutorEnd(queryDesc);
-	    FreeQueryDesc(queryDesc);
-    }
+    // Run the executor
+    ExecutorStart(queryDesc, eflags);
+    // This calls into initPlan() which populates the plan tree.
+    // TODO: Create a hook to executor start.
 
-    // Finally, after performing the extension specific operations, explain the planner.
-    ExplainOnePlan(plan, into, es, queryString, params, queryEnv, &plan_duration, NULL);
+    // Finally, walk through the plan, dumping its out in a separate top-level group.
+    ExplainOpenGroup("TscoutProps", NULL, true, es);
+    WalkPlan(queryDesc->planstate->plan, es);
+    ExplainCloseGroup("TscoutProps", NULL, true, es);
+
+    // Free the created query description resources.
+    ExecutorEnd(queryDesc);
+    FreeQueryDesc(queryDesc);
+  }
+
+  // Finally, after performing the extension specific operations, explain the planner.
+  ExplainOnePlan(plan, into, es, queryString, params, queryEnv, &plan_duration, NULL);
 }
 
 /**
  * @brief Fetches the size of the field
- * 
+ *
  * @param type (c_type) - The C field type
  * @return size_t - Size of the field on the machine
  */
 size_t get_field_size(c_type type) {
-    switch (type)
-    {
+  switch (type) {
     case T_BOOL:
-        return sizeof(bool);
+      return sizeof(bool);
     case T_ENUM:
-        // For now, let's assume that all enumerations are the size of ints.
-        // We're only interested in the NodeTag enum which is an int.
-        // TODO (Karthik): Revisit this.
+      // For now, let's assume that all enumerations are the size of ints.
+      // We're only interested in the NodeTag enum which is an int.
+      // TODO (Karthik): Revisit this.
     case T_INT:
-        return sizeof(int);
+      return sizeof(int);
     case T_SHORT:
-        return sizeof(short);
+      return sizeof(short);
     case T_LONG:
-        return sizeof(long);
+      return sizeof(long);
     case T_DOUBLE:
-        return sizeof(double);
+      return sizeof(double);
     case T_PTR:
-        return sizeof(void *);
+      return sizeof(void *);
     default:
-        break;
-    }
+      break;
+  }
 
-    return 0;
+  return 0;
 }
 
 /**
- * @brief - Explains the features of the given node. 
- * 
+ * @brief - Explains the features of the given node.
+ *
  * @param node (Plan *) - Plan node to be explained.
  * @param es (ExplainState *) - The current EXPLAIN state.
  */
 static void explainXs(Plan *node, ExplainState *es) {
-    char *nodeTagExplainer = NULL, nodeName[17];
-    
-    int i, start_index = 0, field_size, next_field_size, padding, num_fields;
-    field *fields;
+  char *nodeTagExplainer = NULL, nodeName[17];
 
-    // NOTE: It is assumed that ou_list contains definitions for all the node tags.
-    nodeTagExplainer = ou_list[nodeTag(node)].name;
-    num_fields = ou_list[nodeTag(node)].num_xs;
-    fields = ou_list[nodeTag(node)].fields;
-    
-    sprintf(nodeName, "node-%d", node->plan_node_id);
-    ExplainPropertyText("node", nodeName, es);
-    ExplainPropertyText("tag", nodeTagExplainer, es);
+  int i, start_index = 0, field_size, next_field_size, padding, num_fields;
+  field *fields;
 
-    for( i=0; i < num_fields; i++) {
-        field_size = get_field_size( fields[i].type);
-        next_field_size = i < num_fields - 1 ? get_field_size(fields[i+1].type) : 8;
+  // NOTE: It is assumed that ou_list contains definitions for all the node tags.
+  nodeTagExplainer = ou_list[nodeTag(node)].name;
+  num_fields = ou_list[nodeTag(node)].num_xs;
+  fields = ou_list[nodeTag(node)].fields;
 
-        switch (fields[i].type)
-        {
-        case T_BOOL:
-            elog(DEBUG1, "%s: %x", fields[i].name, *(bool *)((char *)(node) + start_index));
-            ExplainPropertyBool(fields[i].name, *(bool *)((char *)(node) + start_index), es);
-            break;
-        
-        case T_INT:
-        case T_ENUM:
-            elog(DEBUG1, "%s: %d", fields[i].name, *(int *)((char *)(node) + start_index));
-            ExplainPropertyInteger(fields[i].name, "units", *(int *)((char *)(node) + start_index), es);
-            break;
-        
-        case T_SHORT:
-            elog(DEBUG1, "%s: %ld", fields[i].name, (int64)*(short *)((char *)(node) + start_index));
-            ExplainPropertyInteger(fields[i].name, "units", (int64)*(short *)((char *)(node) + start_index), es);
-            break;
-        
-        case T_LONG:
-            elog(DEBUG1, "%s: %ld", fields[i].name, (int64)*(long *)((char *)(node) + start_index));
-            ExplainPropertyInteger(fields[i].name, "units", (int64)*(long *)((char *)(node) + start_index), es);
-            break;
-        
-        case T_DOUBLE:
-            elog(DEBUG1, "%s: %lf", fields[i].name, *(double *)((char *)(node) + start_index));
-            ExplainPropertyFloat(fields[i].name, "units", *(double *)((char *)(node) + start_index), 6, es);
-            break;
-        
-        case T_PTR:
-            elog(DEBUG1, "%s: %s", fields[i].name, "<skipped>");
-            ExplainPropertyText(fields[i].name, "<skipped>", es);
-            break;
+  sprintf(nodeName, "node-%d", node->plan_node_id);
+  ExplainPropertyText("node", nodeName, es);
+  ExplainPropertyText("tag", nodeTagExplainer, es);
 
-        default:
-            break;
-        }
+  for (i = 0; i < num_fields; i++) {
+    field_size = get_field_size(fields[i].type);
+    next_field_size = i < num_fields - 1 ? get_field_size(fields[i + 1].type) : 8;
 
-        padding = (next_field_size - ((start_index + field_size) % next_field_size)) % next_field_size;
-        start_index += field_size + padding;
-        elog(DEBUG1, "Padding: %d, start index: %d", padding, start_index);
+    switch (fields[i].type) {
+      case T_BOOL:
+        elog(DEBUG1, "%s: %x", fields[i].name, *(bool *)((char *)(node) + start_index));
+        ExplainPropertyBool(fields[i].name, *(bool *)((char *)(node) + start_index), es);
+        break;
+
+      case T_INT:
+      case T_ENUM:
+        elog(DEBUG1, "%s: %d", fields[i].name, *(int *)((char *)(node) + start_index));
+        ExplainPropertyInteger(fields[i].name, "units", *(int *)((char *)(node) + start_index), es);
+        break;
+
+      case T_SHORT:
+        elog(DEBUG1, "%s: %ld", fields[i].name, (int64) * (short *)((char *)(node) + start_index));
+        ExplainPropertyInteger(fields[i].name, "units", (int64) * (short *)((char *)(node) + start_index), es);
+        break;
+
+      case T_LONG:
+        elog(DEBUG1, "%s: %ld", fields[i].name, (int64) * (long *)((char *)(node) + start_index));
+        ExplainPropertyInteger(fields[i].name, "units", (int64) * (long *)((char *)(node) + start_index), es);
+        break;
+
+      case T_DOUBLE:
+        elog(DEBUG1, "%s: %lf", fields[i].name, *(double *)((char *)(node) + start_index));
+        ExplainPropertyFloat(fields[i].name, "units", *(double *)((char *)(node) + start_index), 6, es);
+        break;
+
+      case T_PTR:
+        elog(DEBUG1, "%s: %s", fields[i].name, "<skipped>");
+        ExplainPropertyText(fields[i].name, "<skipped>", es);
+        break;
+
+      default:
+        break;
     }
+
+    padding = (next_field_size - ((start_index + field_size) % next_field_size)) % next_field_size;
+    start_index += field_size + padding;
+    elog(DEBUG1, "Padding: %d, start index: %d", padding, start_index);
+  }
 }
 
 /**
  * @brief - Walks through the plan tree.
- * 
+ *
  * @param plan (Plan *) - Plan node.
  * @param es (ExplainState *) - The current EXPLAIN state.
  */
 static void WalkPlan(Plan *plan, ExplainState *es) {
-    if (plan == NULL) 
-        return;
+  if (plan == NULL) return;
 
-    // 1. Explain the current node.
-    explainXs(plan, es);
+  // 1. Explain the current node.
+  explainXs(plan, es);
 
-    // 2. Explain the tree rooted in the outer (left) plan.
-    if (plan != NULL && outerPlan(plan) != NULL) {
-        ExplainOpenGroup("left-child", "left-child", true, es);
-        WalkPlan(outerPlan(plan), es);
-        ExplainCloseGroup("left-child", "left-child", true, es);
-    }
+  // 2. Explain the tree rooted in the outer (left) plan.
+  if (plan != NULL && outerPlan(plan) != NULL) {
+    ExplainOpenGroup("left-child", "left-child", true, es);
+    WalkPlan(outerPlan(plan), es);
+    ExplainCloseGroup("left-child", "left-child", true, es);
+  }
 
-	// 3. Explain the tree rooted in the inner (right) plan.
-    if (plan != NULL && innerPlan(plan) != NULL) {
-        ExplainOpenGroup("right-child", "right-child", true, es);
-        WalkPlan(innerPlan(plan), es);
-        ExplainCloseGroup("right-child", "right-child", true, es);
-    }
+  // 3. Explain the tree rooted in the inner (right) plan.
+  if (plan != NULL && innerPlan(plan) != NULL) {
+    ExplainOpenGroup("right-child", "right-child", true, es);
+    WalkPlan(innerPlan(plan), es);
+    ExplainCloseGroup("right-child", "right-child", true, es);
+  }
 
-    // TODO (Karthik): Handle sub-plans.
+  // TODO (Karthik): Handle sub-plans.
 }
--- cmudb/extensions/tscout/operating_unit_codegen.c
+++ cmudb/extensions/tscout/operating_unit_codegen.c
@@ -1,44 +1,44 @@
 /**
  * @brief - An enum that represents all the C data types
  * encountered while scanning the Postgres code-base.
- * 
+ *
  */
 typedef enum c_type {
-    T_BOOL = 0,
-    T_SHORT,
-    T_INT,
-    T_LONG,
-    T_FLOAT,
-    T_DOUBLE,
-    T_ENUM,
-    T_PTR,
-    T_UNKNOWN,
+  T_BOOL = 0,
+  T_SHORT,
+  T_INT,
+  T_LONG,
+  T_FLOAT,
+  T_DOUBLE,
+  T_ENUM,
+  T_PTR,
+  T_UNKNOWN,
 } c_type;
 
 /**
  * @brief - A struct that represents an X or a feature.
  * An X contains a name and a C data type.
- * 
+ *
  */
 typedef struct field {
-    c_type type;
-    char *name;
+  c_type type;
+  char *name;
 } field;
 
 /**
  * @brief - Represents a TScout Operating Unit
- * 
+ *
  */
 typedef struct OperatingUnit {
-    int  ou_index;
-    char *name;
-    int  num_xs;
-    field *fields;
+  int ou_index;
+  char *name;
+  int num_xs;
+  field *fields;
 } ou;
 
 // Features go here
 field feat_none[0];
 
 ou ou_list[] = {
-    (ou){ OU_INDEX, OU_NAME, NUM_Xs, OU_Xs },
+    (ou){OU_INDEX, OU_NAME, NUM_Xs, OU_Xs},
 };
